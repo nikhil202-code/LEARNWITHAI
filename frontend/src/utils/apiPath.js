295c2fff8b0a68bbdbdee0196f92d31560f83acf
Nikhil Anand <nikhilanand2202@gmail.com>
2026-03-09 03:48:16 +0530

    feat: update BASE_URL to use production endpoint and comment out local URL

--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -1,5 +1,5 @@
-export const BASE_URL='http://localhost:8000'
-
+// export const BASE_URL='http://localhost:8000'
+export const BASE_URL='https://learnwithai-gpsu.onrender.com'
 export const API_PATHS={
     AUTH:{
        REGISTER:'/api/auth/register',
